686cccaaa7c38bca0f1b2ee96b990fb0b4c63976
Ping-Che Chen <pcchen@kimicat.com>
2024-11-18 17:04:53 +0800

    Add ChainlinkOracle smart contract.

---
+++ contracts/trading/ChainlinkOracle.sol
@@ -0,0 +1,92 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity =0.8.26;
+
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {IERC20Metadata} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
+import {IAssetOracle} from "../interfaces/trading/IAssetOracle.sol";
+
+
+contract ChainlinkOracle is IAssetOracle, Ownable {
+
+    error InvalidAssetAddress();
+
+    struct OracleInfo {
+        AggregatorV3Interface priceOracle;
+        uint8 priceDecimals;
+        uint8 assetDecimals;
+        uint8 totalDecimals;
+    }
+
+    uint8 private priceDecimals;
+    IERC20Metadata private baseAsset;
+    mapping(IERC20Metadata => OracleInfo) public oracleInfo;
+
+    constructor(
+        address _owner,
+        uint8 _decimals,
+        IERC20Metadata _baseAsset,
+        AggregatorV3Interface _baseOracle
+    ) Ownable(_owner) {
+        priceDecimals = _decimals;
+        baseAsset = _baseAsset;
+        _addAsset(_baseAsset, _baseOracle);
+    }
+
+    function decimals() external view returns (uint8) {
+        return priceDecimals;
+    }
+
+    function getBaseAsset() external view returns (address) {
+        return address(baseAsset);
+    }
+
+    function setAsset(address _asset, AggregatorV3Interface _priceOracle) external onlyOwner {
+        _addAsset(IERC20Metadata(_asset), _priceOracle);
+    }
+
+    function removeAsset(address _asset) external onlyOwner {
+        require(_asset != address(0), InvalidAssetAddress());
+        require(_asset != address(baseAsset), InvalidAssetAddress());
+
+        delete oracleInfo[IERC20Metadata(_asset)];
+    }
+
+    function _addAsset(IERC20Metadata _asset, AggregatorV3Interface _priceOracle) internal {
+        require(address(_asset) != address(0), InvalidAssetAddress());
+
+        OracleInfo storage info = oracleInfo[_asset];
+        info.priceOracle = _priceOracle;
+        info.assetDecimals = _asset.decimals();  // token is assumed to have decimals() function
+        info.priceDecimals = _priceOracle.decimals();
+        info.totalDecimals = info.assetDecimals + info.priceDecimals;
+    }
+
+    function isOracleEnabled(address _asset) external view returns (bool) {
+        return address(oracleInfo[IERC20Metadata(_asset)].priceOracle) != address(0);
+    }
+
+    function getPrice(address _asset) external view returns (uint256 price) {
+        OracleInfo storage assetInfo = oracleInfo[IERC20Metadata(_asset)];
+        require(address(assetInfo.priceOracle) != address(0), AssetNotEnabled());
+
+        if (_asset == address(baseAsset)) {
+            return 10 ** priceDecimals;
+        }
+
+        OracleInfo storage baseInfo = oracleInfo[baseAsset];
+
+        (,int256 assetPrice,,,) = assetInfo.priceOracle.latestRoundData();
+        (,int256 basePrice,,,) = baseInfo.priceOracle.latestRoundData();
+
+        uint256 mulDecimals = baseInfo.totalDecimals + priceDecimals;
+        uint256 divDecimals = assetInfo.totalDecimals;
+        if (mulDecimals > divDecimals) {
+            price = Math.mulDiv(uint256(assetPrice), 10 ** (mulDecimals - divDecimals), uint256(basePrice));
+        }
+        else {
+            price = uint256(assetPrice) / (10 ** (divDecimals - mulDecimals)) / uint256(basePrice);
+        }
+    }
+}
