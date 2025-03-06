4df17952759115bd20bee186c7eadd9f091362ef
Ping-Che Chen <pcchen@kimicat.com>
2025-03-06 15:20:16 +0800

    Update AggregatorHelper to add whitelists.

--- contracts/trading/AggregatorHelper.sol
+++ contracts/trading/AggregatorHelper.sol
@@ -3,6 +3,7 @@
 
 pragma solidity =0.8.26;
 
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {ERC20PermitUpgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20PermitUpgradeable.sol";
 import {IAggregatorHelper} from "../interfaces/trading/IAggregatorHelper.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
@@ -11,12 +12,35 @@ import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol
 /// @notice This contract calls an aggregator for a normal swap input call, and a second call to a
 /// @notice normal swap contract with the remaining tokens to swap the extra dst tokens back to src token,
 /// @notice thus making sure that the amount of dst tokens is exactly the same as specified.
-contract AggregatorHelper is IAggregatorHelper {
+contract AggregatorHelper is IAggregatorHelper, Ownable {
     using SafeERC20 for ERC20PermitUpgradeable;
 
+    error LengthMismatch();
+    error NotWhitelisted();
     error OutputScrapNotCleared();
     error IncorrectScrapAmountSize();
     error IncorrectScrapAmountOffset();
+
+    event SetWhitelist(address sender, address[] router, bool[] isWhitelisted);    
+
+    bool checkWhitelist;
+    mapping(address => bool) public routerWhitelist;    
+    
+    constructor(address initialOwner) Ownable(initialOwner) {
+        checkWhitelist = true;
+    }
+
+    function setWhitelist(address[] calldata _router, bool[] calldata _isWhitelisted) external onlyOwner {
+        if (_router.length != _isWhitelisted.length) revert LengthMismatch();
+
+        for (uint256 i; i < _router.length; ) {
+            routerWhitelist[_router[i]] = _isWhitelisted[i];
+
+            unchecked { ++i; }
+        }
+
+        emit SetWhitelist(msg.sender, _router, _isWhitelisted);
+    }
 
     function swapExactOutput(
         address _src,
@@ -29,6 +53,8 @@ contract AggregatorHelper is IAggregatorHelper {
         bytes calldata _scrapCalldata,
         uint256 _scrapAmountOffset
     ) external {
+        if (checkWhitelist && !(routerWhitelist[_aggregator] && routerWhitelist[_scrapRouter])) revert NotWhitelisted();
+
         ERC20PermitUpgradeable src = ERC20PermitUpgradeable(_src);
         ERC20PermitUpgradeable dst = ERC20PermitUpgradeable(_dst);
         src.safeTransferFrom(msg.sender, address(this), _amountIn);
