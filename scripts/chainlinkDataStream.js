e6fc23e05302d5f5094edb16fe9bb74914847c69
Ping-Che Chen <pcchen@kimicat.com>
2025-07-01 18:21:59 +0800

    Update chainlinkDataStream.js to add open/close position tests.

--- scripts/chainlinkDataStream.js
+++ scripts/chainlinkDataStream.js
@@ -2,6 +2,7 @@
 
 const { ethers } = require("hardhat");
 const crypto = require('crypto');
+const { bigint } = require("hardhat/internal/core/params/argumentTypes");
 
 function loadEnvVar(env, errorMsg) {
     if (env == undefined) {
@@ -10,6 +11,9 @@ function loadEnvVar(env, errorMsg) {
 
     return env;
 }
+
+const ZERO_ADDRESS = '0x' + '0'.repeat(40);
+const UINT256_MAX = '0x' + 'f'.repeat(64);
 
 
 // mainnet
@@ -26,17 +30,33 @@ function loadEnvVar(env, errorMsg) {
 // const chainlinkApiKey = loadEnvVar(process.env.CHAINLINK_API_KEY, "No CHAINLINK_API_KEY");
 // const chainlinkApiSecret = loadEnvVar(process.env.CHAINLINK_API_SECRET, "No CHAINLINK_API_SECRET");
 
+// const TRADING_CORE = '0x99c2901d2883F8D295A989544f118e31eC21823e';
+// const AGGREGATOR_HELPER = '0x0a1E08fF15aD49203dd2566e40623C12380bd1eD';
+// const AGGREGATOR_HELPER_PROCESSOR = '0x11F10a29080A6159628fF8a2587Dd7065ABeE1A6';
+
+// const BASE_TOKEN = '0x3894085Ef7Ff0f0aeDf52E2A2704928d1Ec074F1';
+// const TARGET_TOKEN = '0xE30feDd158A2e3b13e9badaeABaFc5516e95e8C7';    // WSEI
+
 
 // testnet
 const CHAINLINK_FQDN = 'https://api.testnet-dataengine.chain.link';
 const FEED_ID_BASE = '0x0003dc85e8b01946bf9dfd8b0db860129181eb6105a8c8981d9f28e00b6f60d9';  // USDC/USD
 const FEED_ID = '0x0003dba2d8553dfd7afe35c2bfe217ef5106d7805e5272c04a08940ddb868117';  // SEI/USD
-const ASSET_ADDRESS = '0x878aD0bD8DB80A8C6Cc650EdEEd4B9941b571c5F'; // WSEI
 
 const ORACLE_ADDRESS = '0x1e97eb36FdCa3B0705fAB71228E2c92683147B39';
 
 const chainlinkApiKey = loadEnvVar(process.env.CHAINLINK_TEST_API_KEY, "No CHAINLINK_TEST_API_KEY");
 const chainlinkApiSecret = loadEnvVar(process.env.CHAINLINK_TEST_API_SECRET, "No CHAINLINK_TEST_API_SECRET");
+
+const TRADING_CORE = '0xF31900132dFf544Cfe536e76C38a357FF08183D9';
+const SWAP_ROUTER = '0x216d3e7520B09605B7c4243b59aD02Cc6E052F52';
+const AGGREGATOR_HELPER = '0x84cAe8810000AE8Ff8AC3F38452d1f2FB758F1C4';
+const AGGREGATOR_HELPER_PROCESSOR = '0xd130F15d0cC9EcA6916c3f2236B8E786A6919c23';
+
+const BASE_TOKEN = '0x2ed994Fd3DC53bd4010652BFc00D980580823664';
+const TARGET_TOKEN = '0x878aD0bD8DB80A8C6Cc650EdEEd4B9941b571c5F';    // WSEI
+
+const TEST_AMOUNT = 1000000n;
 
 
 function generateHMAC(method, path, body, apiKey, apiSecret) {
@@ -222,32 +242,346 @@ async function fetchReportsTimestamp(feedIds, timestamp, apiKey, apiSecret) {
 }
 
 
+async function initContracts() {
+
+    const tradingCore = await ethers.getContractAt("TradingCore", TRADING_CORE);
+    const baseToken = await ethers.getContractAt("MockToken", BASE_TOKEN);
+    const targetToken = await ethers.getContractAt("MockToken", TARGET_TOKEN);
+
+    return { tradingCore, baseToken, targetToken };
+}
+
+
+// get market contract from baseToken/targetToken pair
+async function getMarket(tradingCore, baseToken, targetToken) {
+    const isBaseToken0 = baseToken.target < targetToken.target;
+    const token0 = isBaseToken0 ? baseToken : targetToken;
+    const token1 = isBaseToken0 ? targetToken : baseToken;
+    const marketAddr = await tradingCore.pairMarket(token0, token1);
+    return await ethers.getContractAt("MarketNFT", marketAddr);
+}
+
+
+// open position to long targetToken
+async function openLongPosition(tradingCore, user, baseToken, targetToken, marginAmount, borrowAmount, swapFunction) {
+    const allowance = await baseToken.allowance(user, tradingCore);
+    if (allowance < marginAmount) {
+        const tx = await baseToken.connect(user).approve(tradingCore, marginAmount);
+        await tx.wait();
+    }
+
+    const market = await getMarket(tradingCore, baseToken, targetToken);
+    const receivedAmount = borrowAmount - (await tradingCore.calculateTradingFee(user, false, borrowAmount));
+    const { swapContract, swapProcessor, swapData } = await swapFunction(true, tradingCore.target, baseToken, targetToken, receivedAmount);
+    return await tradingCore.connect(user).openPosition(
+        market,
+        2,
+        targetToken,
+        marginAmount,
+        borrowAmount,
+        0,
+        UINT256_MAX,
+        0,
+        0,
+        swapContract,
+        swapData
+    );
+}
+
+
+// open position to short targetToken
+async function openShortPosition(tradingCore, user, baseToken, targetToken, marginAmount, borrowAmount, swapFunction) {
+    const allowance = await baseToken.allowance(user, tradingCore);
+    if (allowance < marginAmount) {
+        const tx = await baseToken.connect(user).approve(tradingCore, marginAmount);
+        await tx.wait();
+    }
+
+    const market = await getMarket(tradingCore, baseToken, targetToken);
+    const receivedAmount = borrowAmount - (await tradingCore.calculateTradingFee(user, false, borrowAmount));
+    const { swapContract, swapProcessor, swapData } = await swapFunction(true, tradingCore.target, targetToken, baseToken, receivedAmount);
+    return await tradingCore.connect(user).openPosition(
+        market,
+        2,
+        baseToken,
+        marginAmount,
+        borrowAmount,
+        0,
+        UINT256_MAX,
+        0,
+        0,
+        swapContract,
+        swapData
+    );
+}
+
+
+// close position
+async function closePosition(tradingCore, user, market, positionId, swapFunction) {
+    const token0 = await market.token0();
+    const token1 = await market.token1();
+    const isToken0Margin = await market.isToken0Margin();
+    const baseToken = await ethers.getContractAt("MockToken", isToken0Margin ? token0 : token1);
+    const targetToken = await ethers.getContractAt("MockToken", isToken0Margin ? token1 : token0);
+
+    const positionInfo = await market.getPosition(positionId);
+    const assetAmount = positionInfo.assetAmount;
+
+    const longPosition = positionInfo.isLongToken0 ^ isToken0Margin;
+    if (longPosition) {
+        // for long positions, sell all assets
+        const swappableAmount = await tradingCore.getClosePositionSwappableAfterFee(market, positionId, 0); // for normal closing position
+        const { swapContract, swapProcessor, swapData } = await swapFunction(true, tradingCore.target, targetToken, baseToken, swappableAmount);
+        return await tradingCore.connect(user).closePosition(
+            market,
+            positionId,
+            assetAmount,
+            0,
+            ZERO_ADDRESS,
+            swapContract,
+            swapData
+        );
+    }
+    else {
+        // for short positions, use all swappableAmount to repay all debts
+        const swappableAmount = await tradingCore.getClosePositionSwappableAfterFee(market, positionId, 0);
+        const debtOfPosition = await tradingCore.debtOfPosition(market, positionId);
+        const { swapContract, swapProcessor, swapData } = await swapFunction(false, tradingCore.target, baseToken, targetToken, swappableAmount, debtOfPosition.debtAmount);
+        const assets = assetAmount + positionInfo.marginAmount;
+        return await tradingCore.connect(user).closePosition(
+            market,
+            positionId,
+            assets,
+            0,
+            swapProcessor,
+            swapContract,
+            swapData
+        );
+    }
+}
+
+
+async function testLongPosition(tradingCore, user, baseToken, targetToken, swapper) {
+    console.log("Test long position:");
+    
+    const market = await getMarket(tradingCore, baseToken, targetToken);
+
+    // open position to long targetToken
+    const marginAmount = TEST_AMOUNT;
+    const leverage = 5n;
+    const borrowAmount = marginAmount * leverage;
+    const txOpen = await openLongPosition(tradingCore, user, baseToken, targetToken, marginAmount, borrowAmount, swapper);
+    await txOpen.wait();    
+
+    // get positionId
+    const positions = await market.balanceOf(user);
+    const positionId = await market.tokenOfOwnerByIndex(user, positions - 1n);
+    console.log("Position TokenID:", positionId);
+    const positionInfo = await market.getPosition(positionId);
+    console.log("PositionInfo after open:");
+    console.log(positionInfo);
+    const debtOfPosition = await tradingCore.debtOfPosition(market, positionId);
+    console.log("Debt of position:", debtOfPosition.debtAmount);
+    const liquidationPrice = await tradingCore.getLiquidationPrice(market, positionId);
+    console.log("Liquidation price:", liquidationPrice);
+
+    // close position
+    const txClose = await closePosition(tradingCore, user, market, positionId, swapper);
+    await txClose.wait();
+
+    const positionInfoAfterClose = await market.getPosition(positionId);
+    console.log("PositionInfo after close:");
+    console.log(positionInfoAfterClose);
+    console.log("----------------");
+}
+
+async function testShortPosition(tradingCore, user, baseToken, targetToken, swapper) {
+    console.log("Test short position:");
+
+    const baseDecimals = await baseToken.decimals();
+    const targetDecimals = await targetToken.decimals();
+    const market = await getMarket(tradingCore, baseToken, targetToken);
+
+    // open position to short targetToken
+    const timestamp = Math.floor(Date.now() / 1000) - 2;    // give it a 5 seconds window
+    const prices = await fetchReportsTimestamp([ FEED_ID_BASE, FEED_ID ], timestamp, chainlinkApiKey, chainlinkApiSecret);
+    const marginAmount = TEST_AMOUNT;
+    const leverage = 5n;
+    // NOTE: this assumes the decimals of the Chainlink price reports are the same (generally 18 decimals) which is not necessarily the case
+    const borrowAmount = marginAmount * leverage * 10n ** BigInt(targetDecimals) * prices[0].decodedReport.price / prices[1].decodedReport.price / 10n ** BigInt(baseDecimals);
+    const txOpen = await openShortPosition(tradingCore, user, baseToken, targetToken, marginAmount, borrowAmount, swapper);
+    await txOpen.wait();
+
+    // get positionId
+    const positions = await market.balanceOf(user);
+    const positionId = await market.tokenOfOwnerByIndex(user, positions - 1n);
+    console.log("Position TokenID:", positionId);
+    const positionInfo = await market.getPosition(positionId);
+    console.log("PositionInfo after open:");
+    console.log(positionInfo);
+    const debtOfPosition = await tradingCore.debtOfPosition(market, positionId);
+    console.log("Debt of position:", debtOfPosition.debtAmount);
+    const liquidationPrice = await tradingCore.getLiquidationPrice(market, positionId);
+    console.log("Liquidation price:", liquidationPrice);
+
+    // close position
+    const txClose = await closePosition(tradingCore, user, market, positionId, swapper);
+    await txClose.wait();
+
+    const positionInfoAfterClose = await market.getPosition(positionId);
+    console.log("PositionInfo after close:");
+    console.log(positionInfoAfterClose);    
+    console.log("----------------");
+}
+
+
+// swap data for OracleSwap
+async function oracleCalldata(fromToken, toToken, amount, receiver) {
+    const swapContract = await ethers.getContractAt("OracleSwap", SWAP_ROUTER);
+    const swapProcessor = "";
+    const swapData = swapContract.interface.encodeFunctionData("swapExactInput", [
+        fromToken,
+        toToken,
+        amount,
+        receiver,
+        0n
+    ]);
+    const amountOut = await swapContract.calculateOutAmount(fromToken, toToken, amount);
+
+    return { swapContract, swapProcessor, swapData, amountOut };
+}
+
+
+// Chainlink report data
+async function verifierCalldata() {
+    // fetch multiple reports at a specific timestamp
+    const timestamp = Math.floor(Date.now() / 1000) - 2;    // give it a 2 seconds window
+    const result = await fetchReportsTimestamp([ FEED_ID_BASE, FEED_ID ], timestamp, chainlinkApiKey, chainlinkApiSecret);
+
+    const oracle = await ethers.getContractAt("ChainlinkDataStreamOracle", ORACLE_ADDRESS);
+    const calldata = oracle.interface.encodeFunctionData("verifyReports",
+        [[
+            result[0].response.fullReport,
+            result[1].response.fullReport,
+        ]]
+    );
+
+    return { oracle, calldata };
+}
+
+
+// generate swapData for OracleSwap
+async function oracleSwapper(input, receiver, fromToken, toToken, amount, debtAmount = 0n) {
+    if (input) {
+        const verifierInfo = await verifierCalldata();
+        const swapContract = await ethers.getContractAt("AggregatorHelper", AGGREGATOR_HELPER);
+        const swapInfo = await oracleCalldata(fromToken.target, toToken.target, amount, swapContract.target);
+        const swapProcessor = "";
+        const swapData = swapContract.interface.encodeFunctionData("swapExactInput",
+            [
+                fromToken.target,
+                toToken.target,
+                amount,
+                verifierInfo.oracle.target,
+                verifierInfo.calldata,
+                swapInfo.swapContract.target,
+                swapInfo.swapData
+            ]);
+        return { swapContract, swapProcessor, swapData };
+    }
+    else {
+        const verifierInfo = await verifierCalldata();
+        const swapContract = await ethers.getContractAt("AggregatorHelper", AGGREGATOR_HELPER);
+        const swapProcessor = await ethers.getContractAt("AggregatorHelperProcessor", AGGREGATOR_HELPER_PROCESSOR);
+
+        // trying to estimate how much fromToken is required to received required amount
+        const swapInfo = await oracleCalldata(fromToken.target, toToken.target, amount, swapContract.target);
+        const finalOutputMin = BigInt(swapInfo.amountOut);
+        const aggregatorRouter = swapInfo.swapContract.target;
+
+        // add 2% for safe margin
+        let newAmountIn = amount * 102n * debtAmount / finalOutputMin / 100n;
+        if (newAmountIn > amount) {
+            // should not be over amount
+            newAmountIn = amount;
+        }
+        const newSwapInfo = await oracleCalldata(fromToken.target, toToken.target, newAmountIn, swapContract.target);
+        const newfinalOutputMin = BigInt(newSwapInfo.amountOut);
+
+        if (newfinalOutputMin < debtAmount) {
+            throw Error("Not enough amount in");
+        }
+
+        // use a OracleSwap contract as scrap router
+        const scrapRouter = await ethers.getContractAt("OracleSwap", SWAP_ROUTER);
+        const scrapSwapData = scrapRouter.interface.encodeFunctionData("swapExactInput",
+             [
+                toToken.target,
+                fromToken.target,
+                0,  // amountIn
+                swapContract.target, // receiver
+                0,  // amountOutMin
+            ]);
+
+        const swapData = swapContract.interface.encodeFunctionData("swapExactOutput",
+            [
+                fromToken.target,
+                toToken.target,
+                newAmountIn,
+                debtAmount,
+                verifierInfo.oracle.target,
+                verifierInfo.calldata,
+                aggregatorRouter,
+                newSwapInfo.swapData,
+                scrapRouter.target,
+                scrapSwapData,
+                32 * 2 + 4      // amountIn is the 3th parameter
+            ]);
+
+        return { swapContract, swapProcessor, swapData };
+    }
+}
+
+
 async function main() {
 
+    // test fetch latest single report
     // const result = await fetchSingleReportLatest(FEED_ID, chainlinkApiKey, chainlinkApiSecret);
     // console.log(result);
 
+    // test fetch single report at a specific timestamp
     // const timestamp = Math.floor(Date.now() / 1000) - 5;    // give it a 5 seconds window
     // console.log(timestamp);
     // const result = await fetchSingleReportTimestamp(FEED_ID, timestamp, chainlinkApiKey, chainlinkApiSecret);
     // console.log(result);
 
-    const timestamp = Math.floor(Date.now() / 1000) - 5;    // give it a 5 seconds window
-    console.log(timestamp);
-    const result = await fetchReportsTimestamp([ FEED_ID_BASE, FEED_ID ], timestamp, chainlinkApiKey, chainlinkApiSecret);
-    console.log(result);
+    // test fetch multiple reports at a specific timestamp
+    // const timestamp = Math.floor(Date.now() / 1000) - 5;    // give it a 5 seconds window
+    // console.log(timestamp);
+    // const result = await fetchReportsTimestamp([ FEED_ID_BASE, FEED_ID ], timestamp, chainlinkApiKey, chainlinkApiSecret);
+    // console.log(result);
 
-    // verify reports
-    const oracle = await ethers.getContractAt("ChainlinkDataStreamOracle", ORACLE_ADDRESS);
-    const tx = await oracle.verifyReports([
-        result[0].response.fullReport,
-        result[1].response.fullReport,
-    ]);
-    await tx.wait();
+    // // verify reports
+    // const oracle = await ethers.getContractAt("ChainlinkDataStreamOracle", ORACLE_ADDRESS);
+    // const tx = await oracle.verifyReports([
+    //     result[0].response.fullReport,
+    //     result[1].response.fullReport,
+    // ]);
+    // await tx.wait();
 
-    // get price from oracle
-    const price = await oracle.getPrice(ASSET_ADDRESS);
-    console.log("Price:", price);
+    // // get price from oracle
+    // const price = await oracle.getPrice(ASSET_ADDRESS);
+    // console.log("Price:", price);
+
+    // test open/close positions using Chainlink price reports
+    const [user] = await ethers.getSigners();
+    const { tradingCore, baseToken, targetToken } = await initContracts();
+
+    // test open and close long position
+    await testLongPosition(tradingCore, user, baseToken, targetToken, oracleSwapper);
+
+    // test open and close short position
+    await testShortPosition(tradingCore, user, baseToken, targetToken, oracleSwapper);
 }
 
 
